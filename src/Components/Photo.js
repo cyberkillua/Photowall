b5be5247992fe2883b9db6003a30a7424865ed1b
cyberkillua <moshood988@gmail.com>
2020-04-23 20:37:43 +0100

    initial commit

---
+++ src/Components/Photo.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+class Photo extends Component {
+  render() {
+    const post = this.props.post;
+    return (
+      <figure className="figure">
+        <img className="photo" src={post.imageLink} alt={post.description} />
+
+        <figcaption>
+          <p>{post.description} </p>
+        </figcaption>
+        <div className="button-container">
+          <button className="button" onClick = {() => {
+              this.props.onRemovedPhoto(post);
+          }}>Remove </button>
+        </div>
+      </figure>
+    );
+  }
+}
+
+export default Photo;
---
+++ src/Components/Photowall.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import Photo from "./Photo";
+class Photowall extends Component {
+  render() {
+    return (
+      <div className="photoGrid">
+        {this.props.posts.map((post, index) => (
+          <Photo
+            key={index}
+            post={post}
+            onRemovedPhoto={this.props.onRemovedPhoto}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Photowall;
